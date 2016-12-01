f1ed94bc3002469e867d9abcf7807b13805e7a51
ZheCheng Xu <albertxu217@gmail.com>
2016-11-30 17:56:09 -0800

    added bacnet building controls for HVAC and lighting

--- main/forms.py
+++ main/forms.py
@@ -8,7 +8,15 @@ class SubmitForm(forms.Form):
 	amount = forms.IntegerField()
 	energyType = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES)
 
-class ControlForm(forms.Form):
+class HVACForm(forms.Form):
+	CHOICES = (('1', 'Low'), ('2', 'High'))
+
+	hvacLoc = forms.IntegerField()
+	lowHigh = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES)
 	temperature = forms.IntegerField()
-	
+
+class LightingForm(forms.Form):
+
+	lightingZoneNumber = forms.IntegerField()
+	brightness = forms.IntegerField()
 	
--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import View
 from . import services
 from . import bacnet
-from .forms import SubmitForm, ControlForm
+from .forms import SubmitForm, HVACForm, LightingForm
 
 # Create your views here.
 class MainView(View):
@@ -18,21 +18,78 @@ class MainView(View):
 		return render(request, self.template_name, {'electricity': electricity, 'water': water, 'electricityPercentage' : ePercent, 'waterPercentage': wPercent})
 
 class ControlFormView(View):
-	form_class = ControlForm
+	form_class1 = HVACForm
+	form_class2 = LightingForm
 	template_name = 'main/control.html'
 	
 	def get(self, request):
-		form = self.form_class(None)
-		return render(request, self.template_name)
+
+		passIn = {}
+	
+		northTemp = bacnet.getHVAC("north", "default")
+		northLow = bacnet.getHVAC("north", "low")
+		northHigh = bacnet.getHVAC("north", "high")
+
+		westTemp = bacnet.getHVAC("west", "default")
+		westLow = bacnet.getHVAC("west", "low")
+		westHigh = bacnet.getHVAC("west", "high")
+
+		passIn['northTemp'] = northTemp
+		passIn['westTemp'] = westTemp
+
+		passIn['northLow'] = northLow
+		passIn['westLow'] = westLow
+
+		passIn['northHigh'] = northHigh
+		passIn['westHigh'] = westHigh
+
+		lighting1 = bacnet.getLighting(1)
+		lighting2 = bacnet.getLighting(2)
+		lighting3 = bacnet.getLighting(3)
+		lighting4 = bacnet.getLighting(4)
+		lighting5 = bacnet.getLighting(5)
+		lighting6 = bacnet.getLighting(6)
+		lighting7 = bacnet.getLighting(7)
+		lighting8 = bacnet.getLighting(8)
+
+		passIn['lighting1'] = lighting1
+		passIn['lighting2'] = lighting2
+		passIn['lighting3'] = lighting3
+		passIn['lighting4'] = lighting4
+		passIn['lighting5'] = lighting5
+		passIn['lighting6'] = lighting6
+		passIn['lighting7'] = lighting7
+		passIn['lighting8'] = lighting8
+
+		return render(request, self.template_name, passIn)
 
 	def post(self, request):
-		form = self.form_class(request.POST)
 
-		if form.is_valid():
-			temperature = form.cleaned_data['temperature']
-			result = bacnet.set_pi_bacnet(bacnet.zone_west_high, temperature)
+		form = ""
 
-			return redirect('main:index')
+		if(request.POST.get('HVACSubmit')):
+			form = self.form_class1(request.POST)
+
+			if form.is_valid():
+				loc = form.cleaned_data['hvacLoc']
+				lowHigh = form.cleaned_data['lowHigh']
+				temp = form.cleaned_data['temperature']
+
+				result = bacnet.setHVAC(loc, lowHigh, temp)
+
+				return redirect('main:index')
+
+		if(request.POST.get('LightingSubmit')):
+			form = self.form_class2(request.POST)
+
+			if form.is_valid():
+				zoneNum = form.cleaned_data['lightingZoneNumber']
+				brightness = form.cleaned_data['brightness']
+
+				result = bacnet.setLighting(zoneNum, brightness)
+
+				return redirect('main:index')
+
 		return render(request, self.template_name, {'form': form})
 
 
